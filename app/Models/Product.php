4c59c673dd5ee1a420866de06763d9d14b75484a
Aleksey <badproper98@gmail.com>
2023-11-06 19:26:16 +0300

    добавил softDelete

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,16 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'name',
         'vendor_code',
         'price',
     ];
+
+    protected $dates = ['deleted_at'];
 
     /**
      * Категории, принадлежащие продуктам.
